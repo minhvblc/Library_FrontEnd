1383cf56f8e95f0fdb6da712aeef4128622b7509
Đức Minh Nguyễn <minhtiensinhvblc@gmail.com>
2021-08-01 22:44:26 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -5,14 +5,10 @@ import React from "react";
 import SignUp from './view/SignUp';
 import Login from './view/logIn';
 import HomePage from './view/HomePage';
+import NavigationBar from "./view/NavigationBar";
 function App() {
   return (
-    <div style = {{
-        position: 'absolute', 
-        left: '50%', 
-        top: '50%',
-        transform: 'translate(-50%, -50%)'
-    }}>
+    <div>
        <HomePage/>
     </div>
   
